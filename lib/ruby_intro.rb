635b3d67e039b14cbd9c7dfa7c6c29a1ab88c173
Emily Bernstein <ebernst6@binghamton.edu>
2018-08-31 02:57:17 +0000

    part 1, still needs to be edited. Written with Aditya Tilyalkar.

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,17 +3,49 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  arr.sum
 end
+#puts sum [1,2]
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  if arr.empty?
+    return 0
+  end  
+  if arr.length == 1  
+    return arr[0]
+  end  
+  arr.sort.reverse[0] + arr.sort.reverse[1] 
+  
 end
+
+#puts max_2_sum [7,2,3,4]
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
-end
+  
+  if arr.empty? or arr.length == 1
+    return false
+  end
+  
+  sorted_array = arr.sort.reverse
+  
+  i = 0
 
+  while i < sorted_array.length
+   #find the value that will sum to n
+    needed = n - sorted_array[i]
+    if needed != sorted_array[i] and arr.include? needed  
+      puts sorted_array[i].to_s+ "," + needed.to_s
+      return true
+    end
+   i = i.next
+  end
+  false
+end
+puts sum_to_n?([-1, -2, 3, 4, 6, -8], 12)
+#puts sum_to_n?([-1, -2, 3, 4, 5, -8], -3)
+#puts sum_to_n? [1], 1
+#puts sum_to_n?([], 1)
+#puts sum_to_n? [1,2,3,7,9,11], 10
 # Part 2
 
 def hello(name)
